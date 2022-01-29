8d06b9cdd584d07c7db0c777cfdab8b5e88c2347
SukmaJara <sukmajaraa@gmail.com>
2022-01-29 16:32:01 +0700

    register done

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -59,7 +59,7 @@ const Register = () => {
                         response.json()
                         const status = response.status
 
-                        if (status == 200) {
+                        if (status == 201) {
                             navigation.navigate("Login")
                         }
                         else(
